6d02b74071751455d7ad6c54d57d352aba8a2f6c
dalter <dalter35@gmail.com>
2015-02-08 13:28:07 -0500

    First design iteration take 2.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function resizeContainer() {
-	var contentSizer = $(window).height() * .5;
-	var headerFooterSizer = $(window).height() *.25;
-	$("div.header-footer").css("height", headerFooterSizer);
-	$("div.content").css("height", contentSizer);
+	var coverSizer = $(window).height() * .80;
+	$("div.coverPhoto").css("min-height", coverSizer);
+	$("div.whiteOut").css("min-height", coverSizer);
+
 }
 
 $(document).ready(function(){
