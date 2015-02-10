8981d7f7fa647134b9f35d1f6a2b38768c7bc696
dalter <dalter35@gmail.com>
2015-02-09 20:23:45 -0500

    Obligatory full Screen cover photo.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 function resizeContainer() {
-	var coverSizer = $(window).height() * .80;
-	$("div.coverPhoto").css("min-height", coverSizer);
+
+
+	var coverSizer = $(window).height();
+	$("div.coverPhoto").css("height", coverSizer);
+	var innerCoverSizer = coverSizer/2;
+	var innerContentHeight = $("div.content").height()/2;
+	$("div.whiteOut").css("top", innerCoverSizer - innerContentHeight);
+	var contentSizer = $(window).height()/2;
+	$("div.yellow").css("height", contentSizer);
+	$("div.blue").css("height", contentSizer);
 
 }
 
 $(document).ready(function(){
-
+	
 	resizeContainer();
 
 	$(window).resize(function() {
